ee953bb7281575c249b0f8ebe303794b635d23fa
Joy Sengupta <joysengupta252005@gamil.com>
2024-06-07 00:34:04 +0530

    animated the image and added the logo

--- Basics/script.js
+++ Basics/script.js
@@ -1,6 +1,29 @@
-function Exit(){
-    let closeAlert=confirm("Do you really want to close this Window?")
-    if(closeAlert==true){
+function Exit() {
+    let closeAlert = confirm("Do you really want to close this Window?")
+    if (closeAlert == true) {
         window.close()
     }
 }
+
+
+function TypeAnimation() {
+    var typed = new Typed('#element', {
+        strings: ['Hi. My Name is Sagnik Majumder.', 'This is my First HTML Website'],
+        typeSpeed: 50,
+    });
+}
+
+
+function FadeInAnimation(){
+    document.addEventListener('DOMContentLoaded', function () {
+        var image = document.getElementsByClassName('image')[0];
+        image.classList.add('fade-in');
+    });
+}
+
+
+
+
+TypeAnimation()
+FadeInAnimation()
+
